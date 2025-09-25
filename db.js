3326750a7c277f8c1cbf197b804be0259de19609
ApliCalvin <thecalvins.sims@gmail.com>
2025-09-25 10:48:51 +0700

    Fix DB Connection : - change MySQL Library version (different library) - Add connection inside db.js

---
+++ db.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql2');
+            var db = mysql.createConnection({
+                host: "localhost",
+                user: "root", //sesuaikan dengan username di laptop km
+                password: "simbolon", //sesuaikan dengan password di laptop km
+                database: "crud_nodejs" //nama database yang kita buat tadi
+            });
+            db.connect(function(err) {
+                if (err) throw err;
+                console.log("Connected!");
+            });
+            module.exports = db;
+            
